a1274682c2da8453e5e56e1c1ea668daaececb64
Hunter Dyar <bloop@hdyar.com>
2020-03-07 00:21:10 -0500

    bones of actually using the inventory items.
    idk tho

---
+++ Assets/Scripts/Inventory/InventoryUse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Inventory;
+using ScriptableObjectArchitecture;
+using UnityEngine;
+
+public class InventoryUse : MonoBehaviour
+{
+	public InventoryItemCollection inventory;
+
+	public InventoryItem knifeItem;
+	public InventoryItem flashLightItem;
+
+	
+	public void UseItem(InventoryItem item)
+	{
+		if (item == knifeItem)
+		{
+			Stab();
+		}
+	}
+
+	public void Stab()
+	{
+		//STABBY STAB STAB
+	}
+
+}

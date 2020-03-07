61cb435dae63cc1c5114a24db03156d76473d7bb
Hunter Dyar <bloop@hdyar.com>
2020-03-07 00:22:49 -0500

    At Destination Behavior

---
+++ Assets/Scripts/AI/Behaviors/AtDestination.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+using GameplayScripts;
+// ReSharper disable UnassignedField.Global
+
+[TaskCategory("Grid System")]
+public class AtDestination : Conditional
+{
+	// ReSharper disable once MemberCanBePrivate.Global
+	public SharedAgent myAgent;
+	// ReSharper disable once MemberCanBePrivate.Global
+	public AgentStatus status;
+	
+	public override TaskStatus OnUpdate()
+	{
+		return (myAgent.Value.atDestination) ? TaskStatus.Success : TaskStatus.Failure;
+	}
+}
+
+
+		
